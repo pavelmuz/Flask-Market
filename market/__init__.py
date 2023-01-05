1f651b71322eb3960bb9aab6790441c52fecea2b
Pavel Muzalevskiy <pavel.muzalevsky@gmail.com>
2023-01-06 02:44:32 +0300

    first commit - committing all files to the repository

---
+++ market/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = "fae9a46d041ef7a8d7e9722a"
+db = SQLAlchemy(app=app)
+bcrypt = Bcrypt(app )
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+
+
+from market import routes
