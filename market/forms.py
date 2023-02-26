a4fd0569a81cefe76ae62ade3ac4c972dff9908e
pavelmuz <118142265+pavelmuz@users.noreply.github.com>
2023-02-26 19:46:20 +0300

    2nd commit

--- market/forms.py
+++ market/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField
-from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from wtforms import EmailField, PasswordField, StringField, SubmitField
+from wtforms.validators import (DataRequired, Email, EqualTo, Length,
+                                ValidationError)
+
 from market.models import User
 
 
@@ -8,17 +10,24 @@ class RegisterForm(FlaskForm):
     def validate_username(self, username_to_check):
         user = User.query.filter_by(username=username_to_check.data).first()
         if user:
-            raise ValidationError("Username already exists, try a different username.")
+            raise ValidationError(
+                "Username already exists, try a different username.")
 
     def validate_email_address(self, email_address_to_check):
-        email = User.query.filter_by(email_address=email_address_to_check.data).first()
+        email = User.query.filter_by(
+            email_address=email_address_to_check.data).first()
         if email:
-            raise ValidationError("Email already exists, try a different Email.")
+            raise ValidationError(
+                "Email already exists, try a different Email.")
 
-    username: StringField = StringField(label="User Name:", validators=[Length(min=2, max=30), DataRequired()])
-    email_address = EmailField(label="Email Address:", validators=[Email(), DataRequired()])
-    password1 = PasswordField(label="Password:", validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label="Confirm Password:", validators=[EqualTo("password1"), DataRequired()])
+    username = StringField(label="User Name:", validators=[
+                           Length(min=2, max=30), DataRequired()])
+    email_address = EmailField(label="Email Address:", validators=[
+                               Email(), DataRequired()])
+    password1 = PasswordField(label="Password:", validators=[
+                              Length(min=6), DataRequired()])
+    password2 = PasswordField(label="Confirm Password:", validators=[
+                              EqualTo("password1"), DataRequired()])
     submit = SubmitField(label="Create Account")
 
 
